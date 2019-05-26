99ff02a8313307afdf256d8853ea101231781fa9
shenjiaheng <1465799166@qq.com>
2019-05-26 21:47:03 +0800

    add service-detail

--- src/app.js
+++ src/app.js
@@ -20,6 +20,7 @@ class App extends Component {
       'pages/forum/index',
       'pages/user/index',
       'pages/article-detail/index',
+      'pages/service-detail/index',
       'pages/release-demand/index',
       'pages/message/index',
       'pages/chat/index',
